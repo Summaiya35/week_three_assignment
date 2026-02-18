701f9f47c74d537f7f664ebb7268cf9dc7bd1936
Ammarah Mirani <155808418+Summaiya35@users.noreply.github.com>
2026-02-18 22:05:55 +0500

    Update main.jsx
    at first i pushed the my project folder with already done the changes of assignment task then when i tried to pull --> compare changes it shows both main and week_three_branches are identical . Therefore instead of adding new changes of assignment task I removed the already done changes from branch main and kept them same in branch week_three_assignment.

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ColorList from "./list.jsx"
 
-const root=createRoot(document.getElementById("root"));
-/*createRoot(document.getElementById('root')).render(
+
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)*/
+)
 
-root.render(<div>
 
-  <h2>Color List</h2>
-  <ColorList/>
-</div>);
